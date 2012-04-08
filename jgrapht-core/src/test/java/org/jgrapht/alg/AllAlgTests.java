45be79a379d07a71fe4a2cf59a5662e9bfdd8103
John Sichi <jsichi@gmail.com>
2012-04-07 22:01:04 -0700

    Add EdmondsBlossomShrinking algorithm, contributed by Alejandro R. Lopez del Huerto.

--- jgrapht-core/src/test/java/org/jgrapht/alg/AllAlgTests.java
+++ jgrapht-core/src/test/java/org/jgrapht/alg/AllAlgTests.java
@@ -90,6 +90,7 @@ public final class AllAlgTests
         suite.addTest(new TestSuite(HamiltonianCycleTest.class));
         suite.addTest(new TestSuite(KruskalMinimumSpanningTreeTest.class));
         suite.addTest(new TestSuite(StoerWagnerMinimumCutTest.class));
+        suite.addTest(new TestSuite(EdmondsBlossomShrinkingTest.class));
 
         // $JUnit-END$
         return suite;
