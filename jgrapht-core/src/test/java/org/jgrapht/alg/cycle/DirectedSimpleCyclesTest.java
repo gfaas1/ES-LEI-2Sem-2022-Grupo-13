89985afd200926944ebd6f1cc60894f152b3f599
Nikolay Ognyanov <nikolay.ognyanov@gmail.com>
2013-09-09 16:44:53 +0300

    Fixed license info.

--- jgrapht-core/src/test/java/org/jgrapht/alg/cycle/DirectedSimpleCyclesTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/alg/cycle/DirectedSimpleCyclesTest.java
@@ -1,20 +1,41 @@
-/*=============================================================================
-
-    Copyright(©) 2012 Nikolay Ognyanov
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
-
-=============================================================================*/
+/* ==========================================
+ * JGraphT : a free Java graph-theory library
+ * ==========================================
+ *
+ * Project Info:  http://jgrapht.sourceforge.net/
+ * Project Creator:  Barak Naveh (http://sourceforge.net/users/barak_naveh)
+ *
+ * (C) Copyright 2003-2008, by Barak Naveh and Contributors.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc.,
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+ */
+/* -------------------------
+ * DirectedSimpleCycles.java
+ * -------------------------
+ * (C) Copyright 2013, by Nikolay Ognyanov
+ *
+ * Original Author: Nikolay Ognyanov
+ * Contributor(s) :
+ *
+ * $Id$
+ *
+ * Changes
+ * -------
+ * 06-Sep-2013 : Initial revision (NO);
+ */
 package org.jgrapht.alg.cycle;
 
 import static org.junit.Assert.assertTrue;
