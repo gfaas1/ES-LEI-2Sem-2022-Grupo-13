5468011a12459ca35fdd015d291c9a8f0553231c
leo <leo@Leo-Ubuntu-Desktop>
2014-02-10 19:54:50 +0000

    Resolved: Needs a file header (nowadays LGPL+EPL). See other files for example.

--- jgrapht-core/src/main/java/org/jgrapht/alg/NaiveLcaFinder.java
+++ jgrapht-core/src/main/java/org/jgrapht/alg/NaiveLcaFinder.java
@@ -1,3 +1,33 @@
+/* ==========================================
+ * JGraphT : a free Java graph-theory library
+ * ==========================================
+ *
+ * Project Info:  http://org.org.jgrapht.sourceforge.net/
+ * Project Creator:  Barak Naveh (http://sourceforge.net/users/barak_naveh)
+ *
+ * (C) Copyright 2003-2013, by Barak Naveh and Contributors.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation, or (at your option) any
+ * later version.
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation.
+ */
+/* -------------------------
+ * NaiveLcaFinder.java
+ * -------------------------
+ *
+ * Original Author:  Leo Crawford
+ * Contributor(s):
+ *
+ */
+
 package org.jgrapht.alg;
 
 import java.util.Collections;
