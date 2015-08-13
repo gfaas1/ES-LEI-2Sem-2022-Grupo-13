8c43a5a1403dca2041b43ee00856b56c5e3d4dc0
Christophe Thiebaud <christophe.thiebaud@aequologica.net>
2015-08-13 22:50:36 +0200

    switched internals from int[][] to BitSet[] for memory footprint
    Moved all of the work from the constructor to the reduce call
    Added standard JGraphT file headers
    Use Javadoc html conventions
    Remove all commented-out printlns in main class
    left commented-out printlns in test class

--- jgrapht-core/src/main/java/org/jgrapht/alg/TransitiveReduction.java
+++ jgrapht-core/src/main/java/org/jgrapht/alg/TransitiveReduction.java
@@ -1,65 +1,92 @@
+/* ==========================================
+ * JGraphT : a free Java graph-theory library
+ * ==========================================
+ *
+ * Project Info:  http://jgrapht.sourceforge.net/
+ * Project Creator:  Barak Naveh (http://sourceforge.net/users/barak_naveh)
+ *
+ * (C) Copyright 2003-2008, by Barak Naveh and Contributors.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation, or (at your option) any
+ * later version.
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation.
+ */
+/* ----------------------
+ * TransitiveReduction.java
+ * ----------------------
+ *
+ * Original Author:   Christophe Thiebaud
+ * Contributor(s):
+ *
+ * Changes
+ * -------
+ * 13-August-2015: Initial revision (CT);
+ *
+ */
 package org.jgrapht.alg;
 
 import java.util.ArrayList;
+import java.util.BitSet;
 import java.util.List;
 import java.util.Set;
 
 import org.jgrapht.DirectedGraph;
 
 /**
+ * An implementation of Harry Hsu's 
+ * <a href="https://en.wikipedia.org/wiki/Transitive_reduction">transitive
+ * reduction algorithm</a>.
+ * 
+ * <p>
+ * cf. <a href="http://projects.csail.mit.edu/jacm/References/hsu1975:11.html">Harry Hsu. "An algorithm for finding a minimal equivalent graph of a digraph.", Journal of the ACM, 22(1):11-16, January 1975.</a> 
+ * </p>
+ * 
+ * <p>
+ * This is a port from a python example by Michael Clerx, posted as an answer to
+ * a question about <a href=
+ * "http://stackoverflow.com/questions/1690953/transitive-reduction-algorithm-pseudocode">
+ * transitive reduction algorithm pseudocode</a> on
+ * <a href="http://stackoverflow.com">Stack Overflow</a>
+ * </p>
  * 
  * @author Christophe Thiebaud
- * 
- * cf. https://en.wikipedia.org/wiki/Transitive_reduction
- * 
- * port from python example by Michael Clerx, posted as an answer to http://stackoverflow.com/questions/1690953/transitive-reduction-algorithm-pseudocode
- * 
+ * @since Aug 13, 2015
  */
+
 public class TransitiveReduction<V, E> {
 
-    final private List<V> vertices;
-    final private int [][] pathMatrix;
-    
-    private final DirectedGraph<V, E> graph;
-    
-    public TransitiveReduction(DirectedGraph<V, E> graph) {
-        super();
-        this.graph = graph;
-        this.vertices = new ArrayList<V>(graph.vertexSet());
-        int n = vertices.size();
-        int[][] original = new int[n][n];
-
-        // initialize matrix with zeros
-        // --> 0 is the default value for int arrays
-        
-        // initialize matrix with edges
-        Set<E> edges = graph.edgeSet();
-        for (E edge : edges) {
-            V v1 = graph.getEdgeSource(edge);
-            V v2 = graph.getEdgeTarget(edge);
-
-            int v_1 = vertices.indexOf(v1);
-            int v_2 = vertices.indexOf(v2);
-
-            original[v_1][v_2] = 1;
-        }
-        
-        this.pathMatrix = original;
-        transformToPathMatrix(this.pathMatrix);
-    }
-    
-    // (package visible for unit testing)
-    static void transformToPathMatrix(int[][] matrix) {
-        // compute path matrix 
+    /**
+     * The matrix passed as input parameter will be transformed into a path
+     * matrix.
+     * 
+     * <p>
+     * This method is package visible for unit testing, but it is meant as a
+     * private method.
+     * </p>
+     * 
+     * @param matrix
+     *            the original matrix to transform into a path matrix
+     * 
+     */
+    static void transformToPathMatrix(BitSet[] matrix) {
+        // compute path matrix
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix.length; j++) { 
+            for (int j = 0; j < matrix.length; j++) {
                 if (i == j) {
                     continue;
                 }
-                if (matrix[j][i] > 0 ){
+                if (matrix[j].get(i)) {
                     for (int k = 0; k < matrix.length; k++) {
-                        if (matrix[j][k] == 0) {
-                            matrix[j][k] = matrix[i][k];
+                        if (!matrix[j].get(k)) {
+                            matrix[j].set(k, matrix[i].get(k));
                         }
                     }
                 }
@@ -67,15 +94,27 @@ public class TransitiveReduction<V, E> {
         }
     }
 
-    // (package visible for unit testing)
-    static void transitiveReduction(int[][] pathMatrix) {
+    /**
+     * The path matrix passed as input parameter will be transformed into a
+     * transitively reduced matrix.
+     * 
+     * <p>
+     * This method is package visible for unit testing, but it is meant as a
+     * private method.
+     * </p>
+     * 
+     * @param pathMatrix
+     *            the path matrix to reduce
+     * 
+     */
+    static void transitiveReduction(BitSet[] pathMatrix) {
         // transitively reduce
-        for (int j = 0; j < pathMatrix.length; j++) { 
+        for (int j = 0; j < pathMatrix.length; j++) {
             for (int i = 0; i < pathMatrix.length; i++) {
-                if (pathMatrix[i][j] > 0){
+                if (pathMatrix[i].get(j)) {
                     for (int k = 0; k < pathMatrix.length; k++) {
-                        if (pathMatrix[j][k] > 0) {
-                            pathMatrix[i][k] = 0;
+                        if (pathMatrix[j].get(k)) {
+                            pathMatrix[i].set(k, false);
                         }
                     }
                 }
@@ -83,18 +122,75 @@ public class TransitiveReduction<V, E> {
         }
     }
 
-    public void reduce() {
-        
-        int n = pathMatrix.length;
-        int[][] transitivelyReducedMatrix = new int[n][n];
-        System.arraycopy(pathMatrix, 0, transitivelyReducedMatrix, 0, pathMatrix.length);
+    /**
+     * This method will remove all transitive edges from the graph passed as
+     * input parameter.
+     * 
+     * <p>
+     * You may want to clone the graph before, as transitive edges will be pitilessly
+     * removed.
+     * <p>
+     * 
+     * <p>
+     * e.g.
+     * 
+     * <pre>
+     * {@code
+     * DirectedGraph<V, T> soonToBePrunedDirectedGraph;
+     * 
+     * new TransitiveReduction<V, T>().reduce(soonToBePrunedDirectedGraph);
+     * 
+     * // pruned !
+     * }
+     * </pre>
+     * </p>
+     * 
+     * @param directedGraph
+     *            the directed graph that will be reduced transitively
+     */
+    public void reduce(final DirectedGraph<V, E> directedGraph) {
+
+        final List<V> vertices = new ArrayList<V>(directedGraph.vertexSet());
+
+        final int n = vertices.size();
+
+        BitSet[] originalMatrix = new BitSet[n];
+        for (int i = 0; i < originalMatrix.length; i++) {
+            originalMatrix[i] = new BitSet(n);
+        }
+
+        // initialize matrix with zeros
+        // 'By default, all bits in the set initially have the value false.'
+        // cf. http://docs.oracle.com/javase/7/docs/api/java/util/BitSet.html
+
+        // initialize matrix with edges
+        final Set<E> edges = directedGraph.edgeSet();
+        for (final E edge : edges) {
+            final V v1 = directedGraph.getEdgeSource(edge);
+            final V v2 = directedGraph.getEdgeTarget(edge);
+
+            final int v_1 = vertices.indexOf(v1);
+            final int v_2 = vertices.indexOf(v2);
+
+            originalMatrix[v_1].set(v_2);
+        }
+
+        // create path matrix from original matrix
+        final BitSet[] pathMatrix = originalMatrix;
+
+        transformToPathMatrix(pathMatrix);
+
+        // create reduced matrix from path matrix
+        final BitSet[] transitivelyReducedMatrix = pathMatrix;
+
         transitiveReduction(transitivelyReducedMatrix);
-        
-        for (int i = 0; i <n; i++) {
-            for (int j = 0; j < n; j++) { 
-                if (transitivelyReducedMatrix[i][j] == 0) {
-                    // System.out.println("removing "+vertices.get(i)+" -> "+vertices.get(j));
-                    graph.removeEdge(graph.getEdge(vertices.get(i), vertices.get(j)));
+
+        // remove edges from the DirectedGraph which are not in the reduced
+        // matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!transitivelyReducedMatrix[i].get(j)) {
+                    directedGraph.removeEdge(directedGraph.getEdge(vertices.get(i), vertices.get(j)));
                 }
             }
         }
--- jgrapht-core/src/test/java/org/jgrapht/alg/TransitiveReductionTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/alg/TransitiveReductionTest.java
@@ -1,25 +1,50 @@
+/* ==========================================
+ * JGraphT : a free Java graph-theory library
+ * ==========================================
+ *
+ * Project Info:  http://jgrapht.sourceforge.net/
+ * Project Creator:  Barak Naveh (http://sourceforge.net/users/barak_naveh)
+ *
+ * (C) Copyright 2003-2008, by Barak Naveh and Contributors.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation, or (at your option) any
+ * later version.
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation.
+ */
+/* ----------------------
+ * TransitiveReductionTest.java
+ * ----------------------
+ *
+ * Original Author:   Christophe Thiebaud
+ * Contributor(s):
+ *
+ * Changes
+ * -------
+ * 13-August-2015: Initial revision (CT);
+ *
+ */
 package org.jgrapht.alg;
 
 import java.util.Arrays;
+import java.util.BitSet;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-/**
- * 
- * @author Christophe Thiebaud
- * 
- * cf. https://en.wikipedia.org/wiki/Transitive_reduction
- * 
- * port from python example by Michael Clerx, posted as an answer to http://stackoverflow.com/questions/1690953/transitive-reduction-algorithm-pseudocode
- * 
- */
 public class TransitiveReductionTest {
-
     @Test
     public void test() {
 
-        int[][] matrix = new int[][] {
+        // @formatter:off
+        final int[][] matrix = new int[][] {
             {0, 1, 1, 0, 0},
             {0, 0, 0, 0, 0},
             {0, 0, 0, 1, 1},
@@ -27,7 +52,7 @@ public class TransitiveReductionTest {
             {0, 1, 0, 0, 0}
         };
         
-        int[][] expected_path_matrix = new int[][] {
+        final int[][] expected_path_matrix = new int[][] {
             {0, 1, 1, 1, 1},
             {0, 0, 0, 0, 0},
             {0, 1, 0, 1, 1},
@@ -35,37 +60,75 @@ public class TransitiveReductionTest {
             {0, 1, 0, 0, 0}
         };
         
-        int[][] expected_transitively_reduced_matrix = new int[][] {
+        final int[][] expected_transitively_reduced_matrix = new int[][] {
             {0, 0, 1, 0, 0},
             {0, 0, 0, 0, 0},
             {0, 0, 0, 1, 0},
             {0, 0, 0, 0, 1},
             {0, 1, 0, 0, 0}
         };
-        
+        // @formatter:on
+
         // System.out.println(Arrays.deepToString(matrix) + " original matrix");
 
-        int n = matrix.length;
+        final int n = matrix.length;
 
         // calc path matrix
         int[][] path_matrix = new int[n][n];
         {
-            System.arraycopy(matrix, 0, path_matrix, 0, matrix.length);
-            
-            TransitiveReduction.transformToPathMatrix(path_matrix);
+            {
+                System.arraycopy(matrix, 0, path_matrix, 0, matrix.length);
+
+                final BitSet[] pathMatrixAsBitSetArray = asBitSetArray(path_matrix);
+
+                TransitiveReduction.transformToPathMatrix(pathMatrixAsBitSetArray);
+
+                path_matrix = asIntArray(pathMatrixAsBitSetArray);
+            }
             // System.out.println(Arrays.deepToString(path_matrix) + " path matrix");
+
             Assert.assertArrayEquals(expected_path_matrix, path_matrix);
         }
 
         // calc transitive reduction
         {
             int[][] transitively_reduced_matrix = new int[n][n];
-            System.arraycopy(path_matrix, 0, transitively_reduced_matrix, 0, matrix.length);
-            
-            TransitiveReduction.transitiveReduction(transitively_reduced_matrix);
+            {
+                System.arraycopy(path_matrix, 0, transitively_reduced_matrix, 0, matrix.length);
+
+                final BitSet[] transitivelyReducedMatrixAsBitSetArray = asBitSetArray(transitively_reduced_matrix);
+
+                TransitiveReduction.transitiveReduction(transitivelyReducedMatrixAsBitSetArray);
+
+                transitively_reduced_matrix = asIntArray(transitivelyReducedMatrixAsBitSetArray);
+            }
+
             // System.out.println(Arrays.deepToString(transitively_reduced_matrix) + " transitive reduction");
+
             Assert.assertArrayEquals(expected_transitively_reduced_matrix, transitively_reduced_matrix);
         }
+    }
+
+    static private BitSet[] asBitSetArray(final int[][] intArray) {
+        final BitSet[] ret = new BitSet[intArray.length];
+        for (int i = 0; i < ret.length; i++) {
+            ret[i] = new BitSet(intArray[i].length);
+            for (int j = 0; j < intArray[i].length; j++) {
+                ret[i].set(j, intArray[i][j] == 1);
+            }
+        }
+        return ret;
+    }
+
+    static private int[][] asIntArray(final BitSet[] bitsetArray) {
+        final int[][] ret = new int[bitsetArray.length][bitsetArray.length];
+        for (int i = 0; i < ret.length; i++) {
+            for (int j = 0; j < ret.length; j++) {
+                ret[i][j] = bitsetArray[i].get(j) ? 1 : 0;
+            }
+        }
+        return ret;
+
     }
 
 }
