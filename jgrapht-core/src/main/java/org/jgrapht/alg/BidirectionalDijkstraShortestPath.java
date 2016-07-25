0bd07d9a07422ba0b1a1bdfdde3e716f4386b60b
Dimitrios Michail <dimitrios.michail@gmail.com>
2016-07-25 15:20:29 +0300

    Made all helper classes inner in order to not pollute the namespace.

--- jgrapht-core/src/main/java/org/jgrapht/alg/BidirectionalDijkstraShortestPath.java
+++ jgrapht-core/src/main/java/org/jgrapht/alg/BidirectionalDijkstraShortestPath.java
@@ -128,11 +128,8 @@ public final class BidirectionalDijkstraShortestPath<V, E>
             throw new IllegalArgumentException("Radius must be non-negative");
         }
 
-        this.path = new BidirectionalDijkstraShortestPathDetails<V, E>(
-            graph,
-            startVertex,
-            endVertex,
-            radius).run();
+        this.path = new AlgorithmDetails(graph, startVertex, endVertex, radius)
+            .run();
     }
 
     /**
@@ -183,6 +180,9 @@ public final class BidirectionalDijkstraShortestPath<V, E>
      * @param endVertex the vertex at which the path should end
      *
      * @return List of edges, or null if no path exists
+     * 
+     * @param <V> the graph vertex type
+     * @param <E> the graph edge type
      */
     public static <V, E> List<E> findPathBetween(
         Graph<V, E> graph,
@@ -195,307 +195,272 @@ public final class BidirectionalDijkstraShortestPath<V, E>
             endVertex).getPathEdgeList();
     }
 
-}
-
-/**
- * The implementation details
- */
-class BidirectionalDijkstraShortestPathDetails<V, E>
-{
-    private final SearchFrontier<V, E> forwardFrontier;
-    private final SearchFrontier<V, E> backwardFrontier;
-
-    private final V source;
-    private final V target;
-    private final double radius;
-
-    public BidirectionalDijkstraShortestPathDetails(
-        Graph<V, E> graph,
-        V source,
-        V target,
-        double radius)
+    /**
+     * The implementation details
+     */
+    class AlgorithmDetails
     {
-        this.forwardFrontier = new SearchFrontier<>(graph);
-        if (graph instanceof DirectedGraph) {
-            this.backwardFrontier = new SearchFrontier<>(
-                new EdgeReversedGraph<>(((DirectedGraph<V, E>) graph)));
-        } else {
-            this.backwardFrontier = new SearchFrontier<>(graph);
+
+        private final SearchFrontier forwardFrontier;
+        private final SearchFrontier backwardFrontier;
+
+        private final V source;
+        private final V target;
+        private final double radius;
+
+        public AlgorithmDetails(
+            Graph<V, E> graph,
+            V source,
+            V target,
+            double radius)
+        {
+            this.forwardFrontier = new SearchFrontier(graph);
+            if (graph instanceof DirectedGraph) {
+                this.backwardFrontier = new SearchFrontier(
+                    new EdgeReversedGraph<>(((DirectedGraph<V, E>) graph)));
+            } else {
+                this.backwardFrontier = new SearchFrontier(graph);
+            }
+            this.source = source;
+            this.target = target;
+            this.radius = radius;
         }
-        this.source = source;
-        this.target = target;
-        this.radius = radius;
-    }
 
-    public GraphPath<V, E> run()
-    {
-        // handle special case if source equals target
-        if (source.equals(target)) {
+        public GraphPath<V, E> run()
+        {
+            // handle special case if source equals target
+            if (source.equals(target)) {
+                return new GraphWalk<>(
+                    forwardFrontier.graph,
+                    source,
+                    target,
+                    Collections.singletonList(source),
+                    Collections.emptyList(),
+                    0d);
+            }
+
+            assert !source.equals(target);
+
+            // initialize both frontiers
+            forwardFrontier.updateDistance(source, null, 0d);
+            backwardFrontier.updateDistance(target, null, 0d);
+
+            // initialize best path
+            double bestPath = Double.POSITIVE_INFINITY;
+            V bestPathCommonVertex = null;
+
+            SearchFrontier frontier = forwardFrontier;
+            SearchFrontier otherFrontier = backwardFrontier;
+
+            while (true) {
+                // stopping condition
+                if (frontier.heap.isEmpty() || otherFrontier.heap.isEmpty()
+                    || frontier.heap.min().getKey()
+                        + otherFrontier.heap.min().getKey() >= bestPath)
+                {
+                    break;
+                }
+
+                // frontier scan
+                FibonacciHeapNode<QueueEntry> node = frontier.heap.removeMin();
+                V v = node.getData().v;
+                double vDistance = node.getKey();
+
+                for (E e : frontier.specifics.edgesOf(v)) {
+
+                    V u = Graphs.getOppositeVertex(frontier.graph, e, v);
+
+                    double eWeight = frontier.graph.getEdgeWeight(e);
+
+                    frontier.updateDistance(u, e, vDistance + eWeight);
+
+                    // check path with u's distance from the other frontier
+                    double pathDistance = vDistance + eWeight
+                        + otherFrontier.getDistance(u);
+
+                    if (pathDistance < bestPath) {
+                        bestPath = pathDistance;
+                        bestPathCommonVertex = u;
+                    }
+
+                }
+
+                // swap frontiers
+                SearchFrontier tmpFrontier = frontier;
+                frontier = otherFrontier;
+                otherFrontier = tmpFrontier;
+
+            }
+
+            // create path if found
+            if (Double.isFinite(bestPath) && bestPath <= radius) {
+                return createPath(bestPath, bestPathCommonVertex);
+            }
+
+            return null;
+        }
+
+        private GraphPath<V, E> createPath(double weight, V commonVertex)
+        {
+            LinkedList<E> edgeList = new LinkedList<>();
+            LinkedList<V> vertexList = new LinkedList<>();
+
+            // add common vertex
+            vertexList.add(commonVertex);
+
+            // traverse forward path
+            V v = commonVertex;
+            while (true) {
+                E e = forwardFrontier.getTreeEdge(v);
+
+                if (e == null) {
+                    break;
+                }
+
+                edgeList.addFirst(e);
+                v = Graphs.getOppositeVertex(forwardFrontier.graph, e, v);
+                vertexList.addFirst(v);
+            }
+
+            // traverse reverse path
+            v = commonVertex;
+            while (true) {
+                E e = backwardFrontier.getTreeEdge(v);
+
+                if (e == null) {
+                    break;
+                }
+
+                edgeList.addLast(e);
+                v = Graphs.getOppositeVertex(backwardFrontier.graph, e, v);
+                vertexList.addLast(v);
+            }
+
             return new GraphWalk<>(
                 forwardFrontier.graph,
                 source,
                 target,
-                Collections.singletonList(source),
-                Collections.emptyList(),
-                0d);
+                vertexList,
+                edgeList,
+                weight);
         }
 
-        assert !source.equals(target);
+        /**
+         * Helper class to maintain the search frontier
+         */
+        class SearchFrontier
+        {
+            final Graph<V, E> graph;
+            final Specifics specifics;
 
-        // initialize both frontiers
-        forwardFrontier.updateDistance(source, null, 0d);
-        backwardFrontier.updateDistance(target, null, 0d);
+            final FibonacciHeap<QueueEntry> heap;
+            final Map<V, FibonacciHeapNode<QueueEntry>> seen;
 
-        // initialize best path
-        double bestPath = Double.POSITIVE_INFINITY;
-        V bestPathCommonVertex = null;
-
-        SearchFrontier<V, E> frontier = forwardFrontier;
-        SearchFrontier<V, E> otherFrontier = backwardFrontier;
-
-        while (true) {
-            // stopping condition
-            if (frontier.heap.isEmpty() || otherFrontier.heap.isEmpty()
-                || frontier.heap.min().getKey()
-                    + otherFrontier.heap.min().getKey() >= bestPath)
+            public SearchFrontier(Graph<V, E> graph)
             {
-                break;
-            }
-
-            // frontier scan
-            FibonacciHeapNode<QueueEntry<V, E>> node = frontier.heap
-                .removeMin();
-            V v = node.getData().v;
-            double vDistance = node.getKey();
-
-            for (E e : frontier.specifics.edgesOf(v)) {
-
-                V u = Graphs.getOppositeVertex(frontier.graph, e, v);
-
-                double eWeight = frontier.graph.getEdgeWeight(e);
-
-                frontier.updateDistance(u, e, vDistance + eWeight);
-
-                // check path with u's distance from the other frontier
-                double pathDistance = vDistance + eWeight
-                    + otherFrontier.getDistance(u);
-
-                if (pathDistance < bestPath) {
-                    bestPath = pathDistance;
-                    bestPathCommonVertex = u;
+                this.graph = graph;
+                if (graph instanceof DirectedGraph) {
+                    this.specifics = new DirectedSpecifics(
+                        (DirectedGraph<V, E>) graph);
+                } else {
+                    this.specifics = new UndirectedSpecifics(graph);
                 }
-
+                this.heap = new FibonacciHeap<>();
+                this.seen = new HashMap<>();
             }
 
-            // swap frontiers
-            SearchFrontier<V, E> tmpFrontier = frontier;
-            frontier = otherFrontier;
-            otherFrontier = tmpFrontier;
-
-        }
-
-        // create path if found
-        if (Double.isFinite(bestPath) && bestPath <= radius) {
-            return createPath(bestPath, bestPathCommonVertex);
-        }
-
-        return null;
-    }
-
-    private GraphPath<V, E> createPath(double weight, V commonVertex)
-    {
-        LinkedList<E> edgeList = new LinkedList<>();
-        LinkedList<V> vertexList = new LinkedList<>();
-
-        // add common vertex
-        vertexList.add(commonVertex);
-
-        // traverse forward path
-        V v = commonVertex;
-        while (true) {
-            E e = forwardFrontier.getTreeEdge(v);
-
-            if (e == null) {
-                break;
+            public void updateDistance(V v, E e, double distance)
+            {
+                FibonacciHeapNode<QueueEntry> node = seen.get(v);
+                if (node == null) {
+                    node = new FibonacciHeapNode<>(new QueueEntry(e, v));
+                    heap.insert(node, distance);
+                    seen.put(v, node);
+                } else {
+                    if (distance < node.getKey()) {
+                        heap.decreaseKey(node, distance);
+                        node.getData().e = e;
+                    }
+                }
             }
 
-            edgeList.addFirst(e);
-            v = Graphs.getOppositeVertex(forwardFrontier.graph, e, v);
-            vertexList.addFirst(v);
-        }
-
-        // traverse reverse path
-        v = commonVertex;
-        while (true) {
-            E e = backwardFrontier.getTreeEdge(v);
-
-            if (e == null) {
-                break;
+            public double getDistance(V v)
+            {
+                FibonacciHeapNode<QueueEntry> node = seen.get(v);
+                if (node == null) {
+                    return Double.POSITIVE_INFINITY;
+                } else {
+                    return node.getKey();
+                }
             }
 
-            edgeList.addLast(e);
-            v = Graphs.getOppositeVertex(backwardFrontier.graph, e, v);
-            vertexList.addLast(v);
+            public E getTreeEdge(V v)
+            {
+                FibonacciHeapNode<QueueEntry> node = seen.get(v);
+                if (node == null) {
+                    return null;
+                } else {
+                    return node.getData().e;
+                }
+            }
+
         }
 
-        return new GraphWalk<>(
-            forwardFrontier.graph,
-            source,
-            target,
-            vertexList,
-            edgeList,
-            weight);
-    }
-
-}
-
-/**
- * Helper class to maintain the search frontier
- */
-class SearchFrontier<V, E>
-{
-    final Graph<V, E> graph;
-    final Specifics<V, E> specifics;
-
-    final FibonacciHeap<QueueEntry<V, E>> heap;
-    final Map<V, FibonacciHeapNode<QueueEntry<V, E>>> seen;
-
-    public SearchFrontier(Graph<V, E> graph)
-    {
-        this.graph = graph;
-        if (graph instanceof DirectedGraph) {
-            this.specifics = new DirectedSpecifics<>(
-                (DirectedGraph<V, E>) graph);
-        } else {
-            this.specifics = new UndirectedSpecifics<>(graph);
+        abstract class Specifics
+        {
+            public abstract Set<? extends E> edgesOf(V vertex);
         }
-        this.heap = new FibonacciHeap<>();
-        this.seen = new HashMap<>();
-    }
 
-    public void updateDistance(V v, E e, double distance)
-    {
-        FibonacciHeapNode<QueueEntry<V, E>> node = seen.get(v);
-        if (node == null) {
-            node = new FibonacciHeapNode<>(new QueueEntry<>(e, v));
-            heap.insert(node, distance);
-            seen.put(v, node);
-        } else {
-            if (distance < node.getKey()) {
-                heap.decreaseKey(node, distance);
-                node.getData().e = e;
+        class DirectedSpecifics
+            extends Specifics
+        {
+
+            private DirectedGraph<V, E> graph;
+
+            public DirectedSpecifics(DirectedGraph<V, E> g)
+            {
+                graph = g;
+            }
+
+            @Override
+            public Set<? extends E> edgesOf(V vertex)
+            {
+                return graph.outgoingEdgesOf(vertex);
             }
         }
-    }
 
-    public double getDistance(V v)
-    {
-        FibonacciHeapNode<QueueEntry<V, E>> node = seen.get(v);
-        if (node == null) {
-            return Double.POSITIVE_INFINITY;
-        } else {
-            return node.getKey();
+        class UndirectedSpecifics
+            extends Specifics
+        {
+
+            private Graph<V, E> graph;
+
+            public UndirectedSpecifics(Graph<V, E> g)
+            {
+                graph = g;
+            }
+
+            @Override
+            public Set<E> edgesOf(V vertex)
+            {
+                return graph.edgesOf(vertex);
+            }
         }
-    }
 
-    public E getTreeEdge(V v)
-    {
-        FibonacciHeapNode<QueueEntry<V, E>> node = seen.get(v);
-        if (node == null) {
-            return null;
-        } else {
-            return node.getData().e;
+        class QueueEntry
+        {
+            E e;
+            V v;
+
+            public QueueEntry(E e, V v)
+            {
+                this.e = e;
+                this.v = v;
+            }
         }
-    }
-}
 
-/**
- * Provides unified interface for operations that are different in directed
- * graphs and in undirected graphs.
- */
-abstract class Specifics<V, E>
-{
-
-    /**
-     * Returns the edges outgoing from the specified vertex in case of directed
-     * graph, and the edge touching the specified vertex in case of undirected
-     * graph.
-     *
-     * @param vertex the vertex whose outgoing edges are to be returned.
-     *
-     * @return the edges outgoing from the specified vertex in case of directed
-     *         graph, and the edge touching the specified vertex in case of
-     *         undirected graph.
-     */
-    public abstract Set<? extends E> edgesOf(V vertex);
-}
-
-/**
- * An implementation of {@link Specifics} for a directed graph.
- */
-class DirectedSpecifics<V, E>
-    extends Specifics<V, E>
-{
-
-    private DirectedGraph<V, E> graph;
-
-    /**
-     * Creates a new DirectedSpecifics object.
-     *
-     * @param g the graph for which this specifics object to be created.
-     */
-    public DirectedSpecifics(DirectedGraph<V, E> g)
-    {
-        graph = g;
     }
 
-    @Override
-    public Set<? extends E> edgesOf(V vertex)
-    {
-        return graph.outgoingEdgesOf(vertex);
-    }
-}
-
-/**
- * An implementation of {@link Specifics} in which edge direction (if any) is
- * ignored.
- */
-class UndirectedSpecifics<V, E>
-    extends Specifics<V, E>
-{
-
-    private Graph<V, E> graph;
-
-    /**
-     * Creates a new UndirectedSpecifics object.
-     *
-     * @param g the graph for which this specifics object to be created.
-     */
-    public UndirectedSpecifics(Graph<V, E> g)
-    {
-        graph = g;
-    }
-
-    @Override
-    public Set<E> edgesOf(V vertex)
-    {
-        return graph.edgesOf(vertex);
-    }
-}
-
-/**
- * Private data to associate with each entry in the priority queue.
- */
-class QueueEntry<V, E>
-{
-    E e;
-    V v;
-
-    public QueueEntry(E e, V v)
-    {
-        this.e = e;
-        this.v = v;
-    }
 }
 
 // End BidirectionalDijkstraShortestPath.java
