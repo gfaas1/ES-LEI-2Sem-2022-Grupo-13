67114ba385a0ef4c6cfa0570ec57986f3c28066c
Joris Kinable <j.kinable@gmail.com>
2016-11-21 21:35:34 -0500

    Finished implemeting gusfieldEquivalentFlowTree. Needs testing

--- jgrapht-core/src/main/java/org/jgrapht/alg/flow/GusfieldEquivalentFlowTree.java
+++ jgrapht-core/src/main/java/org/jgrapht/alg/flow/GusfieldEquivalentFlowTree.java
@@ -3,6 +3,7 @@ package org.jgrapht.alg.flow;
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
 import org.jgrapht.alg.interfaces.MaximumFlowAlgorithm;
+import org.jgrapht.alg.interfaces.MinimumSTCutAlgorithm;
 import org.jgrapht.alg.util.ToleranceDoubleComparator;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleWeightedGraph;
@@ -37,22 +38,24 @@ import java.util.*;
  *   publisher={SIAM}
  * }
  *
- * @author Mads Jensen
+ * @author Joris Kinable
  * @since January 2016
  */
 public class GusfieldEquivalentFlowTree<V,E> implements MaximumFlowAlgorithm<V,E>{
 
     private final Graph<V, E> network;
-    /* Used to compare floating point values */
-    private final Comparator<Double> comparator;
+    private final int N;
+//    /* Used to compare floating point values */
+//    private final Comparator<Double> comparator;
+    private final MinimumSTCutAlgorithm<V, E> minimumSTCutAlgorithm;
 
     /* Data structures for computations */
-    List<V> vertexList=new ArrayList<>();
-    Map<V, Integer> indexMap=new HashMap<>();
-    int[] p;
+    private List<V> vertexList=new ArrayList<>();
+    private Map<V, Integer> indexMap=new HashMap<>();
+    private int[] p;
 
     /* Results */
-    double[][] flowMatrix=null;
+    private double[][] flowMatrix=null;
 
 
     private V lastInvokedSource=null;
@@ -63,29 +66,48 @@ public class GusfieldEquivalentFlowTree<V,E> implements MaximumFlowAlgorithm<V,E
     }
 
     public GusfieldEquivalentFlowTree(SimpleWeightedGraph<V, E> network, double epsilon) {
+        this(network, new PushRelabelMFImpl(network, epsilon));
+    }
+
+    public GusfieldEquivalentFlowTree(SimpleWeightedGraph<V, E> network, MinimumSTCutAlgorithm<V,E> minimumSTCutAlgorithm) {
         this.network = network;
-        this.comparator = new ToleranceDoubleComparator(epsilon);
+        this.N=network.vertexSet().size();
+//        this.comparator = new ToleranceDoubleComparator(epsilon);
+        this.minimumSTCutAlgorithm=minimumSTCutAlgorithm;
         vertexList.addAll(network.vertexSet());
         for(int i=0; i<vertexList.size(); i++)
             indexMap.put(vertexList.get(i), i);
     }
 
     private void calculateEquivalentFlowTree(){
+        flowMatrix=new double[N][N];
+        p=new int[N];
 
-    }
+        for(int s=1; s<N; s++){
+            int t=p[s];
+            double flowValue=minimumSTCutAlgorithm.calculateMinCut(vertexList.get(s),vertexList.get(t));
+            Set<V> sourcePartition=minimumSTCutAlgorithm.getSourcePartition(); //Set X in the paper
+            for(int i=s; i<N; i++)
+                if(sourcePartition.contains(vertexList.get(i)) && p[i]==t)
+                    p[i]=s;
 
-    private void populateFlowMatrix(){
-        if(p==null)
-            this.calculateEquivalentFlowTree();
-        flowMatrix=new double[network.vertexSet().size()][network.vertexSet().size()];
+            //populate the flow matrix
+            flowMatrix[s][t]=flowMatrix[t][s]=flowValue;
+            for(int i=0; i<s; i++)
+                if(i != t)
+                    flowMatrix[s][i]=flowMatrix[i][s]=Math.min(flowMatrix[s][t], flowMatrix[t][i]);
+        }
     }
 
     private SimpleWeightedGraph<V,DefaultWeightedEdge> getEquivalentFlowTree(){
-        if(p==null)
+        if(p==null) //Lazy invocation of the algorithm
             this.calculateEquivalentFlowTree();
-        SimpleWeightedGraph<V, DefaultWeightedEdge> equivalentFlowTree=new SimpleWeightedGraph<V, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+        SimpleWeightedGraph<V, DefaultWeightedEdge> equivalentFlowTree=new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
         Graphs.addAllVertices(equivalentFlowTree, vertexList);
-
+        for(int i=1; i<p.length; i++){
+            DefaultWeightedEdge e=equivalentFlowTree.addEdge(vertexList.get(i), vertexList.get(p[i]));
+            equivalentFlowTree.setEdgeWeight(e, flowMatrix[i][p[i]]);
+        }
         return equivalentFlowTree;
     }
 
@@ -96,8 +118,8 @@ public class GusfieldEquivalentFlowTree<V,E> implements MaximumFlowAlgorithm<V,E
 
     @Override
     public double calculateMaximumFlow(V source, V sink) {
-        if(flowMatrix==null)
-            this.populateFlowMatrix();
+        if(p==null) //Lazy invocation of the algorithm
+            this.calculateEquivalentFlowTree();
         return flowMatrix[indexMap.get(source)][indexMap.get(sink)];
     }
 
@@ -114,5 +136,20 @@ public class GusfieldEquivalentFlowTree<V,E> implements MaximumFlowAlgorithm<V,E
     @Override
     public V getFlowDirection(E e) {
         throw new UnsupportedOperationException("Flows calculated via Equivalent Flow trees only provide a maximum flow value, not the exact flow per edge/arc.");
+    }
+
+    public static void main(String[] args){
+        SimpleWeightedGraph<Integer, DefaultWeightedEdge> network=new SimpleWeightedGraph<Integer, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+        Graphs.addAllVertices(network, Arrays.asList(0,1,2));
+        network.addEdge(0,1);
+        network.addEdge(1,2);
+        network.addEdge(2,0);
+        GusfieldEquivalentFlowTree<Integer, DefaultWeightedEdge> gusfieldEquivalentFlowTree=new GusfieldEquivalentFlowTree<Integer, DefaultWeightedEdge>(network);
+        for(Integer v1 : network.vertexSet()){
+            for(Integer v2 : network.vertexSet()){
+                if(v1==v2) continue;
+                System.out.println("Max flow "+v1+"-"+v2+": "+gusfieldEquivalentFlowTree.getMaximumFlow(v1, v2));
+            }
+        }
     }
 }
