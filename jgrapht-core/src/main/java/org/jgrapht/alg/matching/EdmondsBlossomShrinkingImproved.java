7c450e78cdde4357a3a1cfa14d61082d88a4b8c0
Joris Kinable <j.kinable@gmail.com>
2017-05-01 00:28:56 -0400

    Added UnionFind to Edmonds blossom shrinking. Needs debugging

--- jgrapht-core/src/main/java/org/jgrapht/alg/matching/EdmondsBlossomShrinkingImproved.java
+++ jgrapht-core/src/main/java/org/jgrapht/alg/matching/EdmondsBlossomShrinkingImproved.java
@@ -105,7 +105,7 @@ public class EdmondsBlossomShrinkingImproved<V, E>
         match = new HashMap<>();
         path = new HashMap<>();
         contracted = new HashMap<>();
-//        uf = new UnionFind<>(graph.vertexSet().size());
+        uf = new UnionFind<>(vertexIndexMap.values());
 
         if(initializer != null)
             this.warmStart(initializer);
@@ -141,51 +141,67 @@ public class EdmondsBlossomShrinkingImproved<V, E>
 
     private int findPath(int root)
     {
+        System.out.println("find path");
         Set<Integer> used = new HashSet<>();
         Queue<Integer> q = new ArrayDeque<>();
 
         // Expand graph back from its contracted state
         path.clear();
         contracted.clear();
+        uf.clear();
 
-        vertexIndexMap.values().forEach(vertex -> contracted.put(vertex, vertex));
+//        vertexIndexMap.values().forEach(vertex -> contracted.put(vertex, vertex));
 
         used.add(root);
         q.add(root);
 
         while (!q.isEmpty()) {
             int v = q.remove();
+            System.out.println("remove q");
 
             for (V w : Graphs.neighborListOf(graph, vertices.get(v))) {
                 int to = vertexIndexMap.get(w);
 
-                if (contracted.get(v) == contracted.get(to) || (match.containsKey(v) && to == match.get(v)))
+//                if (contracted.get(v) == contracted.get(to) || (match.containsKey(v) && to == match.get(v)))
+//                    continue;
+                System.out.println("point 1");
+                if (uf.find(v).equals(uf.find(to)) || (match.containsKey(v) && to == match.get(v)))
                     continue;
+
+                System.out.println("point 2");
 
                 // Check whether we've hit a 'blossom'
                 if ((to == root)
                     || ((match.containsKey(to)) && (path.containsKey(match.get(to)))))
                 {
-                    int stem = lowestCommonAncestor(v, to);
+                    System.out.println("start lca");
+                    int stem = lowestCommonAncestor(v, to, root);
+                    System.out.println("end lca");
 
                     Set<Integer> blossom = new HashSet<>();
 
+                    System.out.println("point 3a");
                     markPath(v, to, stem, blossom);
                     markPath(to, v, stem, blossom);
 
+                    System.out.println("start1");
                     vertexIndexMap.values().stream()
                         .filter(
-                            i -> blossom.contains(contracted.get(i)))
+//                            i -> blossom.contains(contracted.get(i)))
+                                i -> blossom.contains(uf.find(i)))
                         .forEach(i -> {
                             contracted.put(i, stem);
+                            uf.union(stem, i);
                             if (!used.contains(i)) {
                                 used.add(i);
                                 q.add(i);
                             }
                         });
+                    System.out.println("end1");
 
                     // Check whether we've had hit a loop (of even length (!) presumably)
                 } else if (!path.containsKey(to)) {
+                    System.out.println("point 3b");
                     path.put(to, v);
 
                     if (!match.containsKey(to)) {
@@ -197,6 +213,7 @@ public class EdmondsBlossomShrinkingImproved<V, E>
                     used.add(to);
                     q.add(to);
                 }
+                System.out.println("point 4");
             }
         }
         return nil;
@@ -204,28 +221,36 @@ public class EdmondsBlossomShrinkingImproved<V, E>
 
     private void markPath(int v, int child, int stem, Set<Integer> blossom)
     {
-        while (!contracted.get(v).equals(stem)) {
-            blossom.add(contracted.get(v));
-            blossom.add(contracted.get(match.get(v)));
+        System.out.println("start mark path");
+//        while (!contracted.get(v).equals(stem)) {
+        while (uf.find(v)!= stem) {
+            //blossom.add(contracted.get(v));
+            blossom.add(uf.find(v));
+//            blossom.add(contracted.get(match.get(v)));
+            blossom.add(uf.find(match.get(v)));
             path.put(v, child);
             child = match.get(v);
             v = path.get(child);
         }
+        System.out.println("end mark path");
     }
 
-    private int lowestCommonAncestor(int a, int b)
+    private int lowestCommonAncestor(int a, int b, int root)
     {
-        Set<Integer> seen = new HashSet<>();
+        BitSet seen=new BitSet(vertices.size());
         for (;;) {
-            a = contracted.get(a);
-            seen.add(a);
+            //a = contracted.get(a);
+            a = uf.find(a);
+//            System.out.println("lc a: "+a+" expected: "+contracted.get(a));
+            seen.set(a);
             if (!match.containsKey(a)) //We've reached the root of the tree
                 break;
             a = path.get(match.get(a));
         }
         for (;;) {
-            b = contracted.get(b);
-            if (seen.contains(b))
+//            b = contracted.get(b);
+            b=uf.find(b);
+            if (seen.get(b))
                 return b;
             b = path.get(match.get(b));
         }
