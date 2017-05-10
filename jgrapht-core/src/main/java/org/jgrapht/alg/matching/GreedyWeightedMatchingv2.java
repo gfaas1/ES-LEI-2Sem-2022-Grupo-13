03331733f20bc7cc24f4ffe0b350485dee073017
Joris Kinable <j.kinable@gmail.com>
2017-05-10 10:01:49 -0400

    improved greedyWeightedMatching

--- jgrapht-core/src/main/java/org/jgrapht/alg/matching/GreedyWeightedMatchingv2.java
+++ jgrapht-core/src/main/java/org/jgrapht/alg/matching/GreedyWeightedMatchingv2.java
@@ -18,8 +18,14 @@
 package org.jgrapht.alg.matching;
 
 import org.jgrapht.Graph;
+import org.jgrapht.VertexFactory;
 import org.jgrapht.alg.interfaces.MatchingAlgorithm;
 import org.jgrapht.alg.util.ToleranceDoubleComparator;
+import org.jgrapht.generate.GnmRandomGraphGenerator;
+import org.jgrapht.generate.GraphGenerator;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
 
 import java.util.*;
 
@@ -94,14 +100,14 @@ public class GreedyWeightedMatchingv2<V, E>
     @Override
     public Matching<V, E> getMatching()
     {
-        // sort edges in non-decreasing order of weight
+        // sort edges in decreasing order of weight
         // (the lambda uses e1 and e2 in the reverse order on purpose)
         List<E> allEdges = new ArrayList<>(graph.edgeSet());
         Collections.sort(
             allEdges,
             (e1, e2) -> {
-                int degreeE1=graph.degreeOf(graph.getEdgeSource(e1))+graph.degreeOf(graph.getEdgeTarget(e1));
-                int degreeE2=graph.degreeOf(graph.getEdgeSource(e2))+graph.degreeOf(graph.getEdgeTarget(e2));
+                double degreeE1=graph.degreeOf(graph.getEdgeSource(e1))+graph.degreeOf(graph.getEdgeTarget(e1));
+                double degreeE2=graph.degreeOf(graph.getEdgeSource(e2))+graph.degreeOf(graph.getEdgeTarget(e2));
                 return comparator.compare(graph.getEdgeWeight(e2)/degreeE2, graph.getEdgeWeight(e1)/degreeE1);
             });
 
@@ -126,6 +132,71 @@ public class GreedyWeightedMatchingv2<V, E>
 
         // return matching
         return new MatchingImpl<>(graph, matching, matchingWeight);
+    }
+
+    public static void main(String[] args){
+
+        int v2Equalsv1=0;
+        int v2IsWorse=0;
+        int v2IsBetter=0;
+
+        Random random=new Random(1);
+        int vertices=200;
+
+        for(int k=0; k<10000; k++) {
+            int edges=random.nextInt(maxEdges(vertices)/2)+5;
+            System.out.println("k: "+k+" v: "+vertices+" e: "+edges);
+            GraphGenerator<Integer, DefaultWeightedEdge, Integer> generator = new GnmRandomGraphGenerator<>(vertices, edges, 0);
+            IntegerVertexFactory vertexFactory = new IntegerVertexFactory();
+
+            long t3=System.currentTimeMillis();
+            Graph<Integer, DefaultWeightedEdge> graph = new SimpleWeightedGraph<>(DefaultWeightedEdge.class);
+            generator.generateGraph(graph, vertexFactory, null);
+            for(DefaultWeightedEdge e : graph.edgeSet())
+                graph.setEdgeWeight(e, random.nextInt(100));
+            t3=System.currentTimeMillis()-t3;
+
+            GreedyWeightedMatching<Integer, DefaultWeightedEdge> v1=new GreedyWeightedMatching<>(graph);
+            long t1=System.currentTimeMillis();
+            Matching<Integer, DefaultWeightedEdge> m1=v1.getMatching();
+            t1=System.currentTimeMillis()-t1;
+            double w1=m1.getWeight();
+            GreedyWeightedMatchingv2<Integer, DefaultWeightedEdge> v2=new GreedyWeightedMatchingv2<>(graph);
+            long t2=System.currentTimeMillis();
+            Matching<Integer, DefaultWeightedEdge> m2=v2.getMatching();
+            t2=System.currentTimeMillis()-t2;
+            double w2=m2.getWeight();
+            System.out.println("t1: "+t1+" t2: "+t2+" t3: "+t3);
+
+            if(w1==w2)
+                v2Equalsv1++;
+            else if(w2 > w1)
+                v2IsBetter++;
+            else
+                v2IsWorse++;
+
+        }
+
+        System.out.println("v2IsBetter: "+v2IsBetter+" v2IsWorse: "+v2IsWorse+" v2Equalsv1: "+v2Equalsv1);
+
+    }
+
+    private static int maxEdges(int n){
+        if (n % 2 == 0) {
+            return Math.multiplyExact(n / 2, n - 1);
+        } else {
+            return Math.multiplyExact(n, (n - 1) / 2);
+        }
+    }
+
+    public static class IntegerVertexFactory implements VertexFactory<Integer>{
+
+        int id=0;
+
+        @Override
+        public Integer createVertex() {
+            return id++;
+        }
     }
 
 }
