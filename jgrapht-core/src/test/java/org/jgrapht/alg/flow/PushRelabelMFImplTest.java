91a6be84f84aac543fddb1f03eb779cf1a4b6fec
vernemat <Mathilde.Vernet@imag.fr>
2017-06-21 15:53:18 +0200

    new test push relabel in correct test class correct code

--- jgrapht-core/src/test/java/org/jgrapht/alg/flow/PushRelabelMFImplTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/alg/flow/PushRelabelMFImplTest.java
@@ -31,4 +31,26 @@ public class PushRelabelMFImplTest
     {
         return new PushRelabelMFImpl<>(network);
     }
+
+    public void testPushRelabelWithNonIdenticalNode() {
+        SimpleDirectedGraph<String,DefaultEdge> g1 = new SimpleDirectedGraph<String, DefaultEdge>(DefaultEdge.class) ;
+
+        g1.addVertex("v0");
+        g1.addVertex("v1");
+        g1.addVertex("v2");
+        g1.addVertex("v3");
+        g1.addVertex("v4");
+        g1.addEdge("v0","v2");
+        g1.addEdge("v3","v4");
+        g1.addEdge("v1","v0");
+        g1.addEdge("v0","v4");
+        g1.addEdge("v0","v1");
+        g1.addEdge("v2","v1");
+
+        MaximumFlowAlgorithm<String, DefaultEdge> mf1 = new PushRelabelMFImpl<>(g1);
+        String sourceFlow = "v" + new String("v3").substring(1) ;
+        String sinkFlow = "v0" ;
+        double flow = mf1.calculateMaximumFlow(sourceFlow,sinkFlow);
+        assertEquals(0.0, flow);
+    }
 }
