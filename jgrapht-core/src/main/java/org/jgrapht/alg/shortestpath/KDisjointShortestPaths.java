1b31b91b9e489c8730e62093dc9eeeaa02e7b270
mizrachi <assaf.mizrachi@ecitele.com>
2018-01-02 09:58:55 +0200

    First version implementing KShortestPathAlgorithm.

---
+++ jgrapht-core/src/main/java/org/jgrapht/alg/shortestpath/KDisjointShortestPaths.java
@@ -0,0 +1,321 @@
+/*
+ * (C) Copyright 2018-2018, by Assaf Mizrachi and Contributors.
+ *
+ * JGraphT : a free Java graph-theory library
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation, or (at your option) any
+ * later version.
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation.
+ */
+package org.jgrapht.alg.shortestpath;
+
+import java.util.*;
+
+import org.jgrapht.*;
+import org.jgrapht.alg.interfaces.*;
+import org.jgrapht.graph.*;
+
+/**
+ * The algorithm determines the k <em>disjoint</em> shortest simple paths in increasing order of
+ * weight. Weights can be negative (but no negative cycle is allowed). Only directed simple graphs
+ * are allowed. 
+ * <br>
+ * An undirected graph can be transformed to a directed simple graph using the following
+ * transformation:
+ * <br>
+ * For each pair of vertices V, U and an undirected edge {@literal E:V<-->U} with weight W:
+ * <ol>
+ * <li> Create vertices V_in, V_out, U_in and U_out
+ * <li> Create edges:
+ *     <ol>
+ *     <li> {@literal V_out-->U_in}, weight = W;
+ *  <li> {@literal U_out-->V_in}, weight = W;
+ *  <li> {@literal V_in-->V_out}, weight = 0;
+ *  <li> {@literal U_in-->U_out}, weight = 0;
+ *  </ol>
+ * </ol>
+ * <p>
+ * The algorithm is based on the Suurballe (later extended by Bhandari) algorithm, so is to find an
+ * Edge-disjoint shortest paths. In order to find a Vertex-disjoint shortest paths use the
+ * following transformation:
+ * <br>
+ * For each vertex V and a set of incoming edges E_in(V) and outgoing edges E_out(V):
+ * <ol>
+ * <li> Create vertex V_in
+ * <li> For every edge {@literal e:u-->V}, weight=W in E_in(V), create an edge {@literal e_in:u-->V_in}, weight=W
+ * <li> Create vertex V_out
+ * <li> For every edge {@literal e:V-->u}, weight=W in E_out(V), create an edge {@literal e_out:V_out-->u}, weight=W
+ * <li> Create an edge {@literal e_internal:V_in-->V_out} with weight=0.
+ * </ol>
+ *
+ * <p>
+ * The algorithm is running k sequential Bellman-Ford iterations to find the shortest path at each step.
+ * Hence, yielding a complexity of k*O(Bellman-Ford). Also, note that the provided graph is to be modified
+ * (edges and weights) during the path computation.
+ * 
+ * <p>
+ * For further reference, see <a href="https://www.nas.ewi.tudelft.nl/people/Fernando/papers/Wiley.pdf">Farabi Iqbal, Fernando A. Kuipers - 
+ * "Disjoint Paths In Networks"</a> which was the main reference for the code of this class.
+ * 
+ * @see BellmanFordShortestPath
+ *
+ * @author Assaf Mizrachi
+ * @since September 11, 2016
+ * 
+ * @param <V> Vertex
+ * @param <E> Edge
+ */
+public class KDisjointShortestPaths<V, E> implements KShortestPathAlgorithm<V, E> {
+    /**
+     * Graph on which shortest paths are searched.
+     */
+    private Graph<V, E> graph;
+
+    private List<List<E>> pathList;
+
+    private int nPaths;
+
+    /**
+     * Creates an object to calculate k disjoint shortest paths between the start
+     * vertex and others vertices.
+     *
+     * @param graph
+     *            graph on which shortest paths are searched. Note: graph will
+     *            be modified (edges and weights) during the path computation.
+     * @param nPaths
+     *            number of disjoint paths between the start vertex and an end
+     *            vertex.
+     *
+     * @throws NullPointerException
+     *             if the specified graph or startVertex is <code>null</code>.
+     * @throws IllegalArgumentException
+     *             if nPaths is negative or 0.
+     */
+    public KDisjointShortestPaths(Graph<V, E> graph, int nPaths) {
+        assertKShortestPathsFinder(graph, nPaths);
+
+        this.graph = graph;
+        this.nPaths = nPaths;
+    }
+    
+    /**
+     * Returns the k shortest simple paths in increasing order of weight.
+     *
+     * @param startVertex source vertex of the calculated paths.
+     * @param endVertex target vertex of the calculated paths.
+     *
+     * @return list of disjoint paths between the start vertex and the end vertex
+     * 
+     * @throws IllegalArgumentException if the graph does not contain the startVertex or the
+     *         endVertex
+     * @throws IllegalArgumentException if the startVertex and the endVertex are the same vertices
+     * @throws IllegalArgumentException if the graph is undirected.
+     */
+    @Override
+    public List<GraphPath<V, E>> getPaths(V startVertex, V endVertex)
+    {
+        assertGetPaths(startVertex, endVertex);
+        //ensure we have directed weighted graph
+        if (! this.graph.getType().isDirected()) {
+            throw new IllegalArgumentException("Graph must be directed");
+        }
+        this.graph = new AsWeightedGraph<>(graph, new HashMap<>());
+
+        GraphPath<V, E> currentPath;
+        this.pathList = new ArrayList<>();
+        BellmanFordShortestPath<V, E> bellmanFordShortestPath;
+        int cPaths = 1;
+        do {
+            setUp(cPaths);
+            bellmanFordShortestPath = new BellmanFordShortestPath<>(this.graph);
+            currentPath = bellmanFordShortestPath.getPath(startVertex, endVertex);
+            if (currentPath != null) {
+                cPaths++;                
+                pathList.add(currentPath.getEdgeList());
+            }            
+        } while (currentPath != null && cPaths <= this.nPaths);
+
+        return pathList.size() > 0 ? resolvePaths(startVertex, endVertex) : null;
+    }
+
+    /**
+     * Prepares the graph for a search of the next path:
+     * Replacing the edges of the previous path with reversed edges
+     * with negative weight
+     * 
+     * @param cPath the number of the next path to search 
+     */
+    private void setUp(int cPath) {
+        //no setup for first path
+        if (cPath == 1) {
+            return;
+        }
+        
+        V source, target;
+        E reversedEdge;
+        //replace previous path edges with reversed edges with negative weight
+        for (E originalEdge : this.pathList.get(cPath - 2)) {
+            source = graph.getEdgeSource(originalEdge);
+            target = graph.getEdgeTarget(originalEdge);
+            graph.removeEdge(originalEdge);    
+            reversedEdge = graph.addEdge(target, source);
+            graph.setEdgeWeight(reversedEdge, - graph.getEdgeWeight(originalEdge));
+        }
+    }
+    
+    /**
+     * At the end of the search we have list of intermediate paths - not necessarily
+     * disjoint and may contain reversed edges. Here we go over all, removing overlapping
+     * edges and merging them to valid paths (from start to end). Finally, we sort
+     * them according to their weight.
+     * 
+     * @param endVertex the end vertex
+     * 
+     * @return sorted list of disjoint paths from start vertex to end vertex.
+     */
+    private List<GraphPath<V, E>> resolvePaths(V startVertex, V endVertex) {
+        //first we need to remove overlapping edges.        
+        removeOverlappingEdges();
+        
+        //now we might be left with path fragments (not necessarily leading from start to end).
+        //We need to merge them to valid paths.
+        List<GraphPath<V, E>> paths = mergePaths(startVertex, endVertex);
+        
+        //sort paths by overall weight (ascending)
+        Collections.sort(paths, (o1, o2) -> Double.compare(o1.getWeight(), o2.getWeight()));        
+        return paths;
+    }
+    
+    /**
+     * After removing overlapping edges, each path is not necessarily connecting
+     * start to end vertex. Here we connect the path fragments to valid paths
+     * (from start to end).
+     * 
+     * @param endVertex the end vertex
+     * 
+     * @return list of disjoint paths from start to end.
+     */
+    private List<GraphPath<V, E>> mergePaths(V startVertex, V endVertex) {
+        List<GraphPath<V, E>> graphPaths = new ArrayList<GraphPath<V, E>>();
+        for (int i = 0; i < this.pathList.size(); i++) {            
+            V nextHop = startVertex;
+            List<E> mergedPath = new ArrayList<E>();
+            while (! nextHop.equals(endVertex)) {
+                E nextEdge = null;
+                //search an edge connecting current and next hops at any of the paths
+                //once it is found, add it to the current (merged) path and remove it
+                //from the original path so it will not be used anymore.
+                for (List<E> path : pathList) {
+                    boolean connectionFound = false;
+                    Iterator<E> pathIter = path.iterator();
+                    while (pathIter.hasNext()) {
+                        E edge = pathIter.next();
+                        if (graph.getEdgeSource(edge).equals(nextHop) || graph.getEdgeTarget(edge).equals(nextHop)) {
+                            nextEdge = edge;
+                            //remove edge so it will not be used again in any other path.
+                            pathIter.remove();
+                            connectionFound = true;
+                            break;
+                        }
+                    }
+                    if (connectionFound) {
+                        break;
+                    }
+                }
+                if (nextEdge == null) {
+                    throw new IllegalArgumentException("Could not find a path from start to end vertex");
+                }
+                mergedPath.add(nextEdge);
+                nextHop = graph.getEdgeSource(nextEdge).equals(nextHop) ? 
+                        graph.getEdgeTarget(nextEdge) : graph.getEdgeSource(nextEdge);
+            }
+            //path is ready, wrap it up.
+            graphPaths.add(createGraphPath(mergedPath, startVertex, endVertex));
+        }
+        return graphPaths;
+    }
+    
+    /**
+     * Iterating over all paths to removes overlapping edges (contained
+     * in more than single path). At the end of this method, each path
+     * contains unique edges but not necessarily connecting the start
+     * to end vertex.
+     * 
+     */
+    private void removeOverlappingEdges() {
+        Iterator<E> path1Iter, path2Iter;
+        E e1, e2;
+        boolean found;
+        //removing overlapping edges
+        for (List<E> path1 : pathList) {
+            path1Iter = path1.iterator();
+            while (path1Iter.hasNext()) {
+                e1 = path1Iter.next();
+                found = false;
+                for (List<E> path2 : pathList) {
+                    if (path2 == path1) {
+                        continue;
+                    }
+                    path2Iter = path2.iterator();
+                    while (path2Iter.hasNext()) {
+                        e2 = path2Iter.next();
+                        //graph is directed, checking both options.
+                        if ((graph.getEdgeSource(e1).equals(graph.getEdgeSource(e2)) &&
+                                graph.getEdgeTarget(e1).equals(graph.getEdgeTarget(e2))) ||
+                                
+                                (graph.getEdgeSource(e1).equals(graph.getEdgeTarget(e2)) &&
+                                        graph.getEdgeTarget(e1).equals(graph.getEdgeSource(e2)))) {
+                            found = true;
+                            path2Iter.remove();
+                        }
+                    }
+                }
+                if (found) {
+                    path1Iter.remove();
+                }
+            }
+        }
+        
+    }
+
+    private void assertGetPaths( V startVertex, V endVertex) {
+        if (endVertex == null) {
+            throw new NullPointerException("endVertex is null");
+        }
+        if (startVertex == null) {
+            throw new NullPointerException("startVertex is null");
+        }
+        if (endVertex.equals(startVertex)) {
+            throw new IllegalArgumentException("The end vertex is the same as the start vertex!");
+        }
+        if (!this.graph.vertexSet().contains(endVertex)) {
+            throw new IllegalArgumentException("Graph must contain the end vertex!");
+        }
+    }
+
+    private void assertKShortestPathsFinder(Graph<V, E> graph, int nPaths) {
+        if (graph == null) {
+            throw new NullPointerException("graph is null");
+        }        
+        if (nPaths <= 0) {
+            throw new NullPointerException("nPaths is negative or 0");
+        }
+    }
+    
+    private GraphPath<V, E> createGraphPath(List<E> edgeList, V startVertex, V endVertex) {
+        double weight = 0;
+        for (E edge : edgeList) {
+            weight += graph.getEdgeWeight(edge);
+        }
+        return new GraphWalk<>(graph, startVertex, endVertex, edgeList, weight);
+    }
+    
+}
---
+++ jgrapht-core/src/test/java/org/jgrapht/alg/shortestpath/KDisjointShortestPathsTest.java
@@ -0,0 +1,317 @@
+/* ==========================================
+ * JGraphT : a free Java graph-theory library
+ * ==========================================
+ *
+ * Project Info:  http://jgrapht.sourceforge.net/
+ * Project Creator:  Barak Naveh (http://sourceforge.net/users/barak_naveh)
+ *
+ * (C) Copyright 2003-2016, by Barak Naveh and Contributors.
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation, or (at your option) any
+ * later version.
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation.
+ */
+/* -------------------------
+ * .java
+ * -------------------------
+ * (C) Copyright 2007-2016, by France Telecom
+ *
+ * Original Author: Assaf Mizrachi and Contributors.
+ * Contributor(s):
+ *
+ * $Id$
+ *
+ * Changes
+ * -------
+ * 11-Sep-2016 : Initial revision (AM);
+ *
+ */
+package org.jgrapht.alg.shortestpath;
+
+import java.util.List;
+
+import org.jgrapht.EnhancedTestCase;
+import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
+import org.jgrapht.VertexFactory;
+import org.jgrapht.generate.GraphGenerator;
+import org.jgrapht.generate.LinearGraphGenerator;
+import org.jgrapht.generate.RingGraphGenerator;
+import org.jgrapht.graph.DefaultDirectedWeightedGraph;
+import org.jgrapht.graph.DefaultWeightedEdge;
+
+/**
+ * 
+ * Tests for the {@link KDisjointShortestPaths} class.
+ * 
+ * @author Assaf Mizrachi
+ */
+public class KDisjointShortestPathsTest extends EnhancedTestCase {
+
+        
+    /**
+     * Tests single path
+     * 
+     * Edges expected in path
+     * ---------------
+     * {@literal 1 --> 2}
+     */
+    public void testSinglePath() {
+        DefaultDirectedWeightedGraph<Integer, DefaultWeightedEdge> graph = new DefaultDirectedWeightedGraph<>(DefaultWeightedEdge.class);        
+        graph.addVertex(1);
+        graph.addVertex(2);
+        DefaultWeightedEdge edge = graph.addEdge(1, 2);
+        graph.setEdgeWeight(edge, 8);
+        KDisjointShortestPaths<Integer, DefaultWeightedEdge> alg = new KDisjointShortestPaths<>(graph, 5);
+        List<GraphPath<Integer, DefaultWeightedEdge>> pathList = alg.getPaths(1, 2);
+        assertEquals(1, pathList.size());
+        assertEquals(1, pathList.get(0).getLength());
+        assertTrue(pathList.get(0).getEdgeList().contains(edge));
+        assertEquals(new Integer(2), pathList.get(0).getEndVertex());
+//        assertEquals(graph, pathList.get(0).getGraph());
+        assertEquals(new Integer(1), pathList.get(0).getStartVertex());
+        assertEquals(2, pathList.get(0).getVertexList().size());
+        assertTrue(pathList.get(0).getVertexList().contains(1));
+        assertTrue(pathList.get(0).getVertexList().contains(2));
+        assertEquals(pathList.get(0).getWeight(), 8.0);
+    }
+    
+    /**
+     * Tests two disjoint paths from 1 to 3
+     * 
+     * Edges expected in path 1
+     * ---------------
+     * {@literal 1 --> 3}
+     * 
+     * Edges expected in path 2
+     * ---------------
+     * {@literal 1 --> 2}
+     * {@literal 2 --> 3}
+     * 
+     */
+    public void testTwoDisjointPaths() {
+        DefaultDirectedWeightedGraph<Integer, DefaultWeightedEdge> graph = new DefaultDirectedWeightedGraph<>(DefaultWeightedEdge.class);        
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        DefaultWeightedEdge e12 = graph.addEdge(1, 2);
+        DefaultWeightedEdge e23 = graph.addEdge(2, 3);
+        DefaultWeightedEdge e13 = graph.addEdge(1, 3);
+        KDisjointShortestPaths<Integer, DefaultWeightedEdge> alg = new KDisjointShortestPaths<>(graph, 5);
+        
+        List<GraphPath<Integer, DefaultWeightedEdge>> pathList = alg.getPaths(1, 3);
+        
+        assertEquals(2, pathList.size());
+        
+        assertEquals(1, pathList.get(0).getLength());
+        assertEquals(2, pathList.get(1).getLength());
+        
+        assertEquals(1.0, pathList.get(0).getWeight());
+        assertEquals(2.0, pathList.get(1).getWeight());
+        
+        assertTrue(pathList.get(0).getEdgeList().contains(e13));
+        
+        assertTrue(pathList.get(1).getEdgeList().contains(e12));
+        assertTrue(pathList.get(1).getEdgeList().contains(e23));
+        
+                
+    }
+    
+    /**
+     * Tests two joint paths from 1 to 4
+     * 
+     * Edges expected in path 1
+     * ---------------
+     * {@literal 1 --> 2}, w=1
+     * {@literal 2 --> 6}, w=1
+     * {@literal 6 --> 4}, w=1
+     * 
+     * Edges expected in path 2
+     * ---------------
+     * {@literal 1 --> 5}, w=2
+     * {@literal 5 --> 3}, w=2
+     * {@literal 3 --> 4}, w=2
+     * 
+     * Edges expected in no path 
+     * ---------------
+     * {@literal 2 --> 3}, w=3
+     * 
+     */
+    public void testTwoDisjointPaths2() {
+        DefaultDirectedWeightedGraph<Integer, DefaultWeightedEdge> graph = new DefaultDirectedWeightedGraph<>(DefaultWeightedEdge.class);        
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addVertex(4);
+        graph.addVertex(5);
+        graph.addVertex(6);
+        
+        DefaultWeightedEdge e12 = graph.addEdge(1, 2);
+        //this edge should not be used
+        DefaultWeightedEdge e23 = graph.addEdge(2, 3);
+        DefaultWeightedEdge e34 = graph.addEdge(3, 4);
+        DefaultWeightedEdge e15 = graph.addEdge(1, 5);
+        DefaultWeightedEdge e53 = graph.addEdge(5, 3);
+        DefaultWeightedEdge e26 = graph.addEdge(2, 6);
+        DefaultWeightedEdge e64 = graph.addEdge(6, 4);
+        
+        graph.setEdgeWeight(e12, 1);
+        graph.setEdgeWeight(e23, 3);
+        graph.setEdgeWeight(e34, 2);
+        graph.setEdgeWeight(e15, 2);
+        graph.setEdgeWeight(e53, 2);
+        graph.setEdgeWeight(e26, 1);
+        graph.setEdgeWeight(e64, 1);
+        
+        KDisjointShortestPaths<Integer, DefaultWeightedEdge> alg = new KDisjointShortestPaths<>(graph, 5);
+        
+        List<GraphPath<Integer, DefaultWeightedEdge>> pathList = alg.getPaths(1, 4);
+        assertEquals(2, pathList.size());
+        assertEquals(3, pathList.get(0).getLength());
+        assertEquals(3, pathList.get(1).getLength());
+        assertEquals(3.0, pathList.get(0).getWeight());
+        assertEquals(6.0, pathList.get(1).getWeight());
+        
+        assertTrue(pathList.get(0).getEdgeList().contains(e12));
+        assertTrue(pathList.get(0).getEdgeList().contains(e26));
+        assertTrue(pathList.get(0).getEdgeList().contains(e64));
+        
+        assertTrue(pathList.get(1).getEdgeList().contains(e15));
+        assertTrue(pathList.get(1).getEdgeList().contains(e53));
+        assertTrue(pathList.get(1).getEdgeList().contains(e34));
+    }
+    
+    /**
+     * Tests three joint paths from 1 to 5
+     * Edges expected in path 1
+     * ---------------
+     * {@literal 1 --> 4}, w=4
+     * {@literal 4 --> 5}, w=1     
+     * 
+     * Edges expected in path 2
+     * ---------------
+     * {@literal 1 --> 2}, w=1
+     * {@literal 2 --> 5}, w=6
+     * 
+     * Edges expected in path 3
+     * ---------------
+     * {@literal 1 --> 3}, w=4
+     * {@literal 3 --> 5}, w=5
+     * 
+     * Edges expected in no path 
+     * ---------------
+     * {@literal 2 --> 3}, w=1
+     * {@literal 3 --> 4}, w=1
+     */
+    public void testThreeDisjointPaths() {
+        DefaultDirectedWeightedGraph<Integer, DefaultWeightedEdge> graph = new DefaultDirectedWeightedGraph<>(DefaultWeightedEdge.class);        
+        graph.addVertex(1);
+        graph.addVertex(2);
+        graph.addVertex(3);
+        graph.addVertex(4);
+        graph.addVertex(5);
+        
+        DefaultWeightedEdge e12 = graph.addEdge(1, 2);
+        DefaultWeightedEdge e25 = graph.addEdge(2, 5);
+        DefaultWeightedEdge e13 = graph.addEdge(1, 3);
+        DefaultWeightedEdge e35 = graph.addEdge(3, 5);
+        DefaultWeightedEdge e14 = graph.addEdge(1, 4);
+        DefaultWeightedEdge e45 = graph.addEdge(4, 5);
+        DefaultWeightedEdge e23 = graph.addEdge(2, 3);
+        DefaultWeightedEdge e34 = graph.addEdge(3, 4);
+        
+        graph.setEdgeWeight(e12, 1);
+        graph.setEdgeWeight(e25, 6);
+        graph.setEdgeWeight(e13, 4);
+        graph.setEdgeWeight(e35, 5);
+        graph.setEdgeWeight(e14, 4);
+        graph.setEdgeWeight(e45, 1);
+        graph.setEdgeWeight(e23, 1);
+        graph.setEdgeWeight(e34, 1);
+        
+        KDisjointShortestPaths<Integer, DefaultWeightedEdge> alg = new KDisjointShortestPaths<>(graph, 5);
+        
+        List<GraphPath<Integer, DefaultWeightedEdge>> pathList = alg.getPaths(1, 5);
+        assertEquals(3, pathList.size());
+        assertEquals(2, pathList.get(0).getLength());
+        assertEquals(2, pathList.get(1).getLength());
+        assertEquals(2, pathList.get(2).getLength());
+        
+        assertEquals(5.0, pathList.get(0).getWeight());
+        assertEquals(7.0, pathList.get(1).getWeight());
+        assertEquals(9.0, pathList.get(2).getWeight());
+        
+        assertTrue(pathList.get(0).getEdgeList().contains(e14));
+        assertTrue(pathList.get(0).getEdgeList().contains(e45));
+        
+        assertTrue(pathList.get(1).getEdgeList().contains(e12));
+        assertTrue(pathList.get(1).getEdgeList().contains(e25));                       
+        
+        assertTrue(pathList.get(2).getEdgeList().contains(e13));
+        assertTrue(pathList.get(2).getEdgeList().contains(e35));
+    }
+    
+    /**
+     * Only single disjoint path should exist on the line
+     */
+    public void testLinear() {
+        Graph<Integer, DefaultWeightedEdge> graph = new DefaultDirectedWeightedGraph<>(DefaultWeightedEdge.class);  
+        GraphGenerator<Integer, DefaultWeightedEdge, Integer> graphGenerator = new LinearGraphGenerator<>(20);
+        graphGenerator.generateGraph(graph, new VertexFactory<Integer>() {
+            
+            private int i = 1;
+            
+            @Override
+            public Integer createVertex() {
+                return i++;
+            }
+        }, null);
+        
+        KDisjointShortestPaths<Integer, DefaultWeightedEdge> alg = new KDisjointShortestPaths<>(graph, 2);
+        List<GraphPath<Integer, DefaultWeightedEdge>> pathList = alg.getPaths(1, 20);
+        
+        assertEquals(1, pathList.size());
+        assertEquals(19, pathList.get(0).getLength());
+        assertEquals(19.0, pathList.get(0).getWeight());
+        
+        for (int i = 1; i < 21; i++) {
+            assertTrue(pathList.get(0).getVertexList().contains(i));
+        }
+    }
+    
+    /**
+     * Exactly single disjoint path should exist on the ring
+     */
+    public void testRing() {
+        Graph<Integer, DefaultWeightedEdge> graph = new DefaultDirectedWeightedGraph<>(DefaultWeightedEdge.class);  
+        GraphGenerator<Integer, DefaultWeightedEdge, Integer> graphGenerator = new RingGraphGenerator<>(20);
+        graphGenerator.generateGraph(graph, new VertexFactory<Integer>() {
+            
+            private int i = 1;
+            
+            @Override
+            public Integer createVertex() {
+                return i++;
+            }
+        }, null);
+        
+        KDisjointShortestPaths<Integer, DefaultWeightedEdge> alg = new KDisjointShortestPaths<>(graph, 2);
+        List<GraphPath<Integer, DefaultWeightedEdge>> pathList = alg.getPaths(1, 10);
+        
+        assertEquals(1, pathList.size());
+        assertEquals(9, pathList.get(0).getLength());
+        assertEquals(9.0, pathList.get(0).getWeight());
+        
+        for (int i = 1; i < 10; i++) {
+            assertTrue(pathList.get(0).getVertexList().contains(i));
+        }
+    }
+}
