f017e100dbff04641a4c8aea2b5621efcd4129db
John Sichi <jsichi@gmail.com>
2018-05-30 23:52:44 -0700

    Exclude generated markdown in .gitignore

--- etc/expandMarkdown.sh
+++ etc/expandMarkdown.sh
@@ -9,7 +9,7 @@ shopt -s failglob
 pushd ${TRAVIS_BUILD_DIR}/docs/guide-templates
 
 for file in *.md; do
-    hercule ${file} -o ${TRAVIS_BUILD_DIR}/docs/guide/${file}
+    hercule ${file} -o ${TRAVIS_BUILD_DIR}/docs/guide/EX-${file}
 done
 
 popd
