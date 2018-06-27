a4a1c29d0e90feb9efb5613b064f8579d0e38ce5
Alexandru Văleanu <valeanu_alex@yahoo.com>
2018-06-27 21:45:02 +0300

    Update copyrights + docs

--- jgrapht-core/src/main/java/org/jgrapht/alg/scoring/ClusteringCoefficient.java
+++ jgrapht-core/src/main/java/org/jgrapht/alg/scoring/ClusteringCoefficient.java
@@ -1,3 +1,20 @@
+/*
+ * (C) Copyright 2018-2018, by Alexandru Valeanu and Contributors.
+ *
+ * JGraphT : a free Java graph-theory library
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation, or (at your option) any
+ * later version.
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation.
+ */
 package org.jgrapht.alg.scoring;
 
 import org.jgrapht.Graph;
@@ -7,6 +24,30 @@ import org.jgrapht.alg.interfaces.VertexScoringAlgorithm;
 import java.util.*;
 import java.util.stream.Collectors;
 
+/**
+ * Clustering coefficient.
+ *
+ * <p>
+ * Computes the local clustering coefficient of each vertex of a graph. The local clustering coefficient of a
+ * node $v$ is given by the expression: $g(v)= \sum_{s \neq v \neq
+ * t}\frac{\sigma_{st}(v)}{\sigma_{st}}$ where $\sigma_{st}$ is the total number of shortest paths
+ * from node $s$ to node $t$ and $\sigma_{st}(v)$ is the number of those paths that pass through
+ * $v$. For more details see
+ * <a href="https://en.wikipedia.org/wiki/Clustering_coefficient">wikipedia</a>.
+ *
+ * <p>
+ * This implementation also computes the global clustering coefficient as well as the average clustering coefficient.
+ *
+ * <p>
+ * The running time is $O(|V| + |D|^2) where $|V|$ is the number of vertices and $|D|$ is the maximum degree of a
+ * vertex. The space complexity is $O(|V|)$.
+ *
+ * @param <V> the graph vertex type
+ * @param <E> the graph edge type
+ *
+ * @author Alexandru Valeanu
+ * @since June 2018
+ */
 public class ClusteringCoefficient<V, E> implements VertexScoringAlgorithm<V, Double> {
 
     /**
@@ -20,23 +61,33 @@ public class ClusteringCoefficient<V, E> implements VertexScoringAlgorithm<V, Do
     private Map<V, Double> scores;
 
     /**
-     * GlobalClusteringCoefficient
+     * Global Clustering Coefficient
      */
     private boolean computed = false;
     private double globalClusteringCoefficient;
 
+    /**
+     * Average Clustering Coefficient
+     */
     private boolean computedAverage = false;
     private double averageClusteringCoefficient;
 
     /**
-     * Constructor
+     * Construct a new instance
      *
      * @param graph the input graph
+     * @throws NullPointerException if {@code graph} is {@code null}
      */
     public ClusteringCoefficient(Graph<V, E> graph) {
         this.graph = Objects.requireNonNull(graph);
     }
 
+    /**
+     * Computes the global clustering coefficient. The global clustering coefficient $C$ is defined as
+     * $C = 3 \times number\_of\_triangles / number\_of\_triplets$.
+     *
+     * @return the global clustering coefficient
+     */
     public double getGlobalClusteringCoefficient() {
         if (!computed){
             computeGlobalClusteringCoefficient();
@@ -45,6 +96,14 @@ public class ClusteringCoefficient<V, E> implements VertexScoringAlgorithm<V, Do
         return globalClusteringCoefficient;
     }
 
+    /**
+     * Computes the average clustering coefficient. The average clustering coefficient $\={C}$ is defined as
+     * $\={C} = \frac{\sum_{i=1}^{n} C_i}{n}$ where $n$ is the number of vertices.
+     *
+     * Note: the average is $0$ is the graph is empty
+     *
+     * @return the average clustering coefficient
+     */
     public double getAverageClusteringCoefficient(){
         if (graph.vertexSet().size() == 0)
             return 0;
--- jgrapht-core/src/test/java/org/jgrapht/alg/scoring/ClusteringCoefficientTest.java
+++ jgrapht-core/src/test/java/org/jgrapht/alg/scoring/ClusteringCoefficientTest.java
@@ -1,3 +1,20 @@
+/*
+ * (C) Copyright 2018-2018, by Alexandru Valeanu and Contributors.
+ *
+ * JGraphT : a free Java graph-theory library
+ *
+ * This program and the accompanying materials are dual-licensed under
+ * either
+ *
+ * (a) the terms of the GNU Lesser General Public License version 2.1
+ * as published by the Free Software Foundation, or (at your option) any
+ * later version.
+ *
+ * or (per the licensee's choosing)
+ *
+ * (b) the terms of the Eclipse Public License v1.0 as published by
+ * the Eclipse Foundation.
+ */
 package org.jgrapht.alg.scoring;
 
 import org.jgrapht.Graph;
@@ -5,6 +22,11 @@ import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleGraph;
 import org.junit.Test;
 
+/**
+ * Tests for {@link ClusteringCoefficient}
+ *
+ * @author Alexandru Valeanu
+ */
 public class ClusteringCoefficientTest {
 
     @Test
