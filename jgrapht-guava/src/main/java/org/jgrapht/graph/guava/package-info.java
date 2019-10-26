03349716e717fb31fe78d4949a88e2dce4ecf35b
John Sichi <jsichi@gmail.com>
2019-10-26 10:43:55 -0700

    Add links in Guava adapater package-info. (#828)

--- jgrapht-guava/src/main/java/org/jgrapht/graph/guava/package-info.java
+++ jgrapht-guava/src/main/java/org/jgrapht/graph/guava/package-info.java
@@ -1,4 +1,7 @@
 /**
- * Guava adapters.
+ * <a href="https://jgrapht.org/guide/UserOverview#guava-graph-adapter">Guava
+ * adapters</a> allow you to <a
+ * href="https://jgrapht.org/guide/GuavaAdapter">run
+ * JGraphT algorithms directly against Guava's common.graph data structures</a>.
  */
 package org.jgrapht.graph.guava;
