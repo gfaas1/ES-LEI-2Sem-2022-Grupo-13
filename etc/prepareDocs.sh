2c5496e5d53b86b7cbe9520069891173e50e3460
John Sichi <jsichi@gmail.com>
2020-06-17 13:56:06 -0700

    Fix incoming javadoc links.

--- etc/prepareDocs.sh
+++ etc/prepareDocs.sh
@@ -9,3 +9,4 @@ ${TRAVIS_BUILD_DIR}/etc/expandMarkdown.sh
 rm -f ${TRAVIS_BUILD_DIR}/docs/guide/.gitignore
 ${TRAVIS_BUILD_DIR}/etc/downloadJavadoc.sh
 mv ${TRAVIS_BUILD_DIR}/target/site/apidocs ${TRAVIS_BUILD_DIR}/docs/javadoc-SNAPSHOT
+cp -Rfn ${TRAVIS_BUILD_DIR}/docs/javadoc/org.jgrapht.*/org ${TRAVIS_BUILD_DIR}/docs/javadoc
