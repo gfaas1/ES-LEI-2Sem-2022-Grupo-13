63421c0945e0f6b416538006c2b0d44e6c2ff7ed
John Sichi <jsichi@gmail.com>
2020-06-18 20:29:27 -0700

    Proper redirects for pre-module javadoc

--- etc/downloadJavadoc.sh
+++ etc/downloadJavadoc.sh
@@ -4,11 +4,36 @@
 
 set -e
 
+: ${TRAVIS_BUILD_DIR?"variable value required"}
+
 pushd ${TRAVIS_BUILD_DIR}
 
 rm -rf docs/javadoc*
 git clone https://github.com/jgrapht/jgrapht-javadoc.git
 mv jgrapht-javadoc/javadoc* docs
 rm -rf jgrapht-javadoc
+
+emit() {
+    module=$1
+    stripped=${2#"./"}
+    file=${TRAVIS_BUILD_DIR}/docs/javadoc/$stripped
+    mkdir -p $(dirname "$file")
+    echo "---" > $file
+    echo "redirect_to: " https://jgrapht.org/javadoc/$module/$stripped >> $file
+    echo "---" >> $file
+}
+
+export -f emit
+
+# Creates redirects from pre-module javadoc structure to post-module
+pushd docs/javadoc
+for dir in `ls -1 -d org.jgrapht.*`
+do
+    module=${dir%"/"}
+    pushd ${module}
+    find . -name '*.html' -print | xargs -I {} bash -c "emit ${module} {}"
+    popd
+done
+popd
 
 popd
