4705290106d5e5ce8229ce0037e059d60030dc74
constancahtfs <56136814+constancahtfs@users.noreply.github.com>
2022-03-27 00:46:41 +0000

    Update jgrapht-core/src/test/java/org/jgrapht/Grupo13TestsSuite.java
    Co-authored-by: bfgoa1-iscte <100235880+bfgoa1-iscte@users.noreply.github.com>

--- jgrapht-core/src/test/java/org/jgrapht/Grupo13TestsSuite.java
+++ jgrapht-core/src/test/java/org/jgrapht/Grupo13TestsSuite.java
@@ -9,6 +9,7 @@ import org.junit.platform.suite.api.Suite;
         "org.jgrapht.alg.util",
         "org.jgrapht.alg.vertexcover",
         "org.jgrapht.generate",
+        "org.jgrapht.graph",
         "org.jgrapht.interfaces",
         "org.jgrapht.perf",
         "org.jgrapht.traverse",
